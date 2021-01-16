093c2f92e3634d6090b4e23fb7e7bc6c0ee5e3b4
Vadim Kulagin <v.kulagin@cian.ru>
2021-01-16 22:55:12 +0300

    initial commit

---
+++ telegram_bot1/main.py
@@ -0,0 +1,49 @@
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+
+def get_dog_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+
+def get_dog_image_url():
+    allowed_extension = ['jpg', 'jpeg', 'png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_dog_url()
+        file_extension = re.search("([^.]*)$", url).group(1).lower()
+    return url
+
+
+def get_cat_image_url():
+    contents = requests.get('http://aws.random.cat/meow').json()
+    url = contents['file']
+    return url
+
+
+def gav(update, context):
+    url = get_dog_image_url()
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def meow(update, context):
+    url = get_cat_image_url()
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def main():
+    updater = Updater('1557288717:AAEkuzvELvvZzbPNghO_p_12lZVvrzGQvXs', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('random_dog', gav, run_async=True))
+    dp.add_handler(CommandHandler('random_cat', meow, run_async=True))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
